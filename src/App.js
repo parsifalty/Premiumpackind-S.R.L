46f7dd6bdaa4e0ac60056954d93e242f234a5fa4
Pasha Litvak <parsifalty@gmail.com>
2024-06-30 17:08:26 +0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
